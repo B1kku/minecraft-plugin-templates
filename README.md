c757a7915d174feed29427fefbf0467ca9c00ee8
Bikku <bikku+dev@slmail.me>
2025-08-09 14:11:52 +0200

    feat: update template and upgrade to a paper only plugin

--- README.md
+++ README.md
@@ -1,19 +1,8 @@
 This project is intended as a barebones Java plugin template for PaperMC
 - Nothing that is not specifically essential should be added here.
-- Gradle should expand most plugin.yml base variables since it shares most anyways.
-
-There's also a kotlin template on a different branch.
+- Gradle should expand most paper-plugin.yml base variables since it shares most anyways.
 
 ### Setup
 - Delete flake.nix and flake.lock if you're not using nix.
-
-- Replace build.gradle variables to suit your needs, and move your project such that
-your main file is on `src/main/java/your/group/name/projectname/ProjectName.java`.
-
-- Change API version on plugin.yml and the paper repository (Not done by gradle yet).
-
-- Replace the package and class name on your name file and it should be done.
-
-No need to change anything else in plugin.yml since that's somewhat done from build.gradle
-
-If I ever feel like it I might make different branches for more complete templates.
+- Adjust the variables on build.gradle.kts to your own.
+- Move the plugin entry class such that it is found under `group.projectName(as lowercase).projectName`.
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,21 @@
-import xyz.jpenilla.resourcefactory.bukkit.BukkitPluginYaml
-// import xyz.jpenilla.resourcefactory.bukkit.Permission // Example for default permission level
+import xyz.jpenilla.resourcefactory.paper.PaperPluginYaml
+// import xyz.jpenilla.resourcefactory.paper.PaperPluginYaml.Load
 
 plugins {
   id("java-library")
-  // Generates plugin.yml based on the Gradle config
-  id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.1.1"
+  // Generates paper-plugin.yml based on the Gradle config
+  id("xyz.jpenilla.resource-factory-paper-convention") version "1.3.0"
 }
 
 group = "your.group.name"
 version = "0.0.1"
-description = "Asd"
+description = "Some description"
 val projectName = "ProjectName"
+val api = "1.21.8"
+
 
 java {
   toolchain.languageVersion = JavaLanguageVersion.of(21)
-}
-
-dependencies {
-  compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
 }
 
 repositories {
@@ -25,25 +23,27 @@ repositories {
   maven("https://repo.papermc.io/repository/maven-public/")
 }
 
+dependencies {
+  compileOnly("io.papermc.paper:paper-api:${api}-R0.1-SNAPSHOT")
+}
+
 tasks {
   compileJava {
     options.encoding = "UTF-8"
   }
-  bukkitPluginYaml {
+  paperPluginYaml {
+    name = "${projectName}"
     main = "${project.group}.${projectName.lowercase()}.${projectName}"
-    load = BukkitPluginYaml.PluginLoadOrder.STARTUP
-    authors = listOf("Author1")
-    apiVersion = "1.20"
-/* Since the plugin has no documentation here's some examples.
-    commands.create("examplecommand") {
-      description = "Example command description"
-      permission = "projectname.examplepermission"
-    }
-    permissions.create("projectname.examplepermission") {
-      description = "Example permission"
-      // Not really needed since this is the default anyways
-      default = Permission.Default.OP
-    }
- */
+    authors = listOf("ExampleAuthor")
+    version = "${project.version}"
+    apiVersion = "${api}"
+    // Examples for some optional settings
+    // bootstrapper = "${project.group}.${projectName.lowercase()}.Bootstrapper"
+    // prefix = "Example"
+    // val serverDeps = listOf("LuckPerms" to Load.BEFORE)
+    // dependencies {
+    //   serverDeps.forEach { (dep, load) -> server(dep, load)}
+    // }
   }
 }
+
